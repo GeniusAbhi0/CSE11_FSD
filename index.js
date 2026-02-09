170806feaf0e0a7766ae8df86b5200b2d60def5a
Abhijeet <abhijeet2122004@gmail.com>
2026-02-09 15:06:30 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-console.log("Abhijeet");
+//console.log("Abhijeet");
+
+var a =5; 
+let b = 5;
+const c = 5;
+d = 40;
+
+let num = 100;
+let str = "hello world";
+let und;
+let nul = null;
+let  bool =  true;
+{
+let k = 50;
+}
+
+
+function add(p,q){
+    return p+q;
+}
+
+// console.log(add(12,20));
+
+// Arrow function
+
+const multiply = (p,q)=> p*q;
+console.log("arrow function", multiply(5,5));
+
+
+let arr = [10,20,30,40];
+console.log(arr[0]);
+arr.push();
+
+
+let student={
+   name:"Abhijeet",
+   age:20,
+   course:"B.Tech",
+};
+
+let today = new Date();
+
+console.time("test");
+for(let i =0;i<10000;i++){}
+    //console.timeEnd("test");
+
+
+    console.count("a");
+    console.count("a");
+    console.count("a");
+    console.count("a");
+    console.count("a");
+    console.count("a");
+    console.count("b");
+    console.count("a");
+    console.count("a");
+    console.count("a");
+
+    // table
+
+    let students={name: "Abhijeet",age:"50",course:"cse"}
+    console.table(students);
+ 
